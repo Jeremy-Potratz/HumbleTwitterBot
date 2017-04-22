bed7b42df21fefc3babd9f76b17f2d2e6521d88b
Jotto0118 <jpotratz7338@stu.d214.org>
2017-04-21 23:52:24 -0500

    Made some dank changes and have api key

--- bot.js
+++ bot.js
@@ -1,1 +1,6 @@
-console.log('The ot is starting')
+console.log('The bot is starting')
+
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+// username/email: jeremyBotMan011/jeremyottopotratz@gmail.com password: 9Jotto
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'lPLZbQhjJoHyfUJjkh56sLTax',
+  consumer_secret:      'MNELae4176VVsrtwEymFInr9PtvB30GFIbqlVUa3a0fG1rd4g5',
+  access_token:         '855639140007907328-F38AewS44LbBYX43TDf4om66DG1a06R',
+  access_token_secret:  'Juy8D0CyeL5ckr1xC7LoIsKsIuBMC7Bjo58jUuaf7LBXm'
+
+}
